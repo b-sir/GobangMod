401ff1e55d873c06b2fd84382b6af0aa60ddfe8e
zhaobi <me@zhaobi.org>
2020-03-26 18:17:11 +0800

    初始化Mod信息

--- src/main/java/org/zhaobi/gobang/GobangMod.java
+++ src/main/java/org/zhaobi/gobang/GobangMod.java
@@ -1,19 +1,19 @@
-package com.example.examplemod;
+package org.zhaobi.gobang;
+
+import org.apache.logging.log4j.Logger;
 
 import net.minecraft.init.Blocks;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
-import org.apache.logging.log4j.Logger;
 
-@Mod(modid = ExampleMod.MODID, name = ExampleMod.NAME, version = ExampleMod.VERSION)
-public class ExampleMod
-{
-    public static final String MODID = "examplemod";
-    public static final String NAME = "Example Mod";
-    public static final String VERSION = "1.0";
-
+@Mod(modid = GobangMod.MODID, name = GobangMod.NAME, version = GobangMod.VERSION)
+public class GobangMod {
+	public static final String MODID = "gobang";
+    public static final String NAME = "BSir's Gobang Mod";
+    public static final String VERSION = "1.0.0.0";
+    
     private static Logger logger;
 
     @EventHandler
